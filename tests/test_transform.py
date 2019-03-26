aba340867b8fb77f7a62272e6973596199859ce4
Filip Kokosiński <filip.kokosinski@gmail.com>
2019-03-26 21:31:10 +0100

    transform: rework matrix combining

--- tests/test_transform.py
+++ tests/test_transform.py
@@ -1,5 +1,6 @@
 import numpy as np
 from tomograph.transform import projective, cartesian, translate, rotate
+from tomograph.transform import transform_list, transform_apply
 
 
 def test_transform_projective():
@@ -46,7 +47,7 @@ def test_transform_rotate():
     """ Test tomograph.transform.rotate rotation matrix. """
     coords = np.array([100, 50, 1])
     angle = np.pi / 2
-    expected = np.array([50.0, -100.0, 1.0])
+    expected = np.array([-50.0, 100.0, 1.0])
 
     coords = rotate(angle).dot(coords)
     assert np.allclose(coords, expected)
@@ -57,8 +58,43 @@ def test_transform_join():
     coords = np.array([-100, 50, 1])
     move = (-150, 125)
     angle = np.pi / 2
-    expected = np.array([-100.0, 225.0, 1.0])
+    expected = np.array([-200.0, 25.0, 1.0])
 
     coords = rotate(angle).dot(coords)
     coords = translate(*move).dot(coords)
+    assert np.allclose(coords, expected)
+
+
+def test_transform_list():
+    """ Test transforming a list of coordinates. """
+    coords = np.linspace(10, 100, num=10).reshape(5, 2)
+    w = np.array([[1], ]*5)
+    coords = np.append(coords, w, axis=1)
+    t_mat = translate(100, 50)
+    expected = coords + np.array([100, 50, 0])
+
+    coords = transform_list(coords, t_mat)
+    assert np.allclose(coords, expected)
+
+
+def test_transform_apply():
+    """ Test applying list of transformations to a list of coordinates. """
+    coords = np.linspace(10, 100, num=10).reshape(5, 2)
+    w = np.array([[1], ]*5)
+    coords = np.append(coords, w, axis=1)
+    expected = np.array([
+        [90, 30, 1],
+        [70, 10, 1],
+        [50, -10, 1],
+        [30, -30, 1],
+        [10, -50, 1]
+    ])
+
+    t_mat = translate(100, 50)
+    r_mat = rotate(np.pi)
+
+    # rotate first, then translate
+    transformations = [t_mat, r_mat]
+
+    coords = transform_apply(coords, transformations)
     assert np.allclose(coords, expected)
--- tomograph/transform.py
+++ tomograph/transform.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def projective(coords):
     """ Convert 2D cartesian coordinates to homogeneus/projective. """
-    num, _ = np.shape(coords)
+    num = np.shape(coords)[0]
     w = np.array([[1], ]*num)
 
     return np.append(coords, w, axis=1)
@@ -26,7 +26,23 @@ def translate(x, y):
 def rotate(a):
     """ Return rotation matrix.  """
     return np.array([
-        [np.cos(a), np.sin(a), 0],
-        [-np.sin(a), np.cos(a), 0],
+        [np.cos(a), -np.sin(a), 0],
+        [np.sin(a), np.cos(a), 0],
         [0, 0, 1]
     ])
+
+
+def transform_list(coords, matrix):
+    """ Apply transformation to a list of coordinates. """
+    return matrix.dot(coords.T).T
+
+
+def transform_apply(coords, transforms):
+    """ Apply list of transformations to a list of coordinates. """
+    out = coords
+    transforms_r = transforms[::-1]
+
+    for transform in transforms_r:
+        out = transform_list(out, transform)
+
+    return out
